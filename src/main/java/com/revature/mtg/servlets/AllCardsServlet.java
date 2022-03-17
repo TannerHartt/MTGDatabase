8657d7b2d29661c0a5de7e8909c6a4d2cd2aa004
TannerHartt <tannerhart24@yahoo.com>
2022-03-17 01:04:53 -0700

    Beginning stages of button functionality. Fixed search bar icon issue

---
+++ src/main/java/com/revature/mtg/servlets/AllCardsServlet.java
@@ -0,0 +1,8 @@
+package com.revature.mtg.servlets;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class AllCardsServlet extends HttpServlet {
+
+
+}
---
+++ src/main/java/com/revature/mtg/servlets/RandomCardServlet.java
@@ -0,0 +1,7 @@
+package com.revature.mtg.servlets;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class RandomCardServlet extends HttpServlet {
+
+}
